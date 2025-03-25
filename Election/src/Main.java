58376fa85bc25ce68643b60911f5f4621f9b5cd9
Marshall-IronSide <meliodas.martin@gmail.com>
2025-03-25 13:14:40 +0000

    Java project

--- Election/src/Main.java
+++ Election/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        DatabaseManager.creerBaseDeDonnees();
+        Election election = new Election();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== GESTION DES ÉLECTIONS ===");
+        election.saisirCandidats(scanner);
+        election.saisirSuffragesPremierTour(scanner);
+        election.determinerResultatsPremierTour();
+
+        System.out.println("Voulez-vous procéder au second tour ? (Oui/Non)");
+        if (scanner.next().equalsIgnoreCase("Oui")){
+            election.gererSecondTour(scanner);
+        }
+        scanner.close();
     }
 }
