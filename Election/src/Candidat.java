9495c76a7883b39905743bb5cef9a13958c3933f
Marshall-IronSide <meliodas.martin@gmail.com>
2025-03-27 23:46:26 +0000

    Java project

--- Election/src/Candidat.java
+++ Election/src/Candidat.java
@@ -1,28 +1,6 @@
+import javafx.beans.property.*;
 public class Candidat {
-    String nom;
-    int suffragesPremierTour;
-    int suffragesSecondTour;
-    public Candidat(String nom){
-        this.nom=nom;
-        this.suffragesPremierTour=0;
-        this.suffragesSecondTour=0;
-    }
-    public String getNom() {
-        return nom;
-    }
-    public int getSuffragesPremierTour() {
-        return suffragesPremierTour;
-    }
-    public int getSuffragesSecondTour() {
-        return suffragesSecondTour;
-    }
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-    public void setSuffragesPremierTour(int voix) {
-        this.suffragesPremierTour = voix;
-    }
-    public void setSuffragesSecondTour(int voix) {
-        this.suffragesSecondTour = voix;
-    }
+    private final StringProperty nom = new SimpleStringProperty();
+    private final IntegerProperty suffragesPremierTour = new SimpleIntegerProperty();
+
 }
